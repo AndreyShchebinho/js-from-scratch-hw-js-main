dbcb5d1f345e1b433f88b77614c39f7cccb1166b
AndreyShchebinho <shcherbakovandy.s@gmail.com>
2026-04-26 18:48:10 +0300

    complete task-1

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -1,5 +1,14 @@
 // С помощью цикла while найдите сумму чисел от 1 до 20
 // Результат присвойте переменной sum
 
+
 // your code
-let sum
+let sum = 0;
+let i = 1;
+
+while (i <= 20) {
+    sum += i;
+    i++;
+}
+
+console.log(sum); // 210
