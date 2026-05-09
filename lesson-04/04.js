30d7d7fcd537a26f575b5839aee00532b700429f
AndreyShchebinho <shcherbakovandy.s@gmail.com>
2026-05-09 14:47:41 +0300

    complete task 4-4

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,22 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+    let result = '';
+    
+    for (let i = 0; i < str.length; i++) {
+        result = result + str[i] + str[i];
+        // или: result += str[i] + str[i];
+    }
+    
+    return result;
+}
+
+// Проверка
+console.log(doubleEachCharacter('hello'));      // 'hheelllloo'
+console.log(doubleEachCharacter('abc'));        // 'aabbcc'
+console.log(doubleEachCharacter('123'));        // '112233'
+console.log(doubleEachCharacter(''));           // ''
+console.log(doubleEachCharacter('a'));          // 'aa'
+console.log(doubleEachCharacter('Hi!'));        // 'HHii!!'
+
